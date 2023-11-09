9d3fbefb0385a44981b9dbea5b8e226ad1a246ae
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-11-09 21:53:31 +0000

    create loader and add animation, hide and show it

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new_quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // Show New Quote
 function newQuote() {
+    loading();
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
@@ -22,10 +36,14 @@ function newQuote() {
     }
 
     quoteText.textContent = quote.text;
+
+    // Hide Loader
+    complete();
 };
 
 // Get Quotes From API
 async function getQuotes() {
+    loading();
     const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
     
     try {
