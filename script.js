e5694d78d5675fd99e587557b3a8214de027d9d2
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-11-09 21:41:28 +0000

    target elements in JS and manipulate contents

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
+const quoteContainer = document.getElementById("quote_container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new_quote");
+
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+
+    // Check if author field is blank and replace it with "Unknown"
+    authorText.textContent = !quote.author ? "Unknown" : quote.author;
+
+    // Check Quote length to determine styling
+    if (quote.text.length > 50) {
+        quoteText.classList.add("long_quote");
+    } else {
+        quoteText.classList.remove("long_quote");
+    }
+
+    quoteText.textContent = quote.text;
 };
 
 // Get Quotes From API
