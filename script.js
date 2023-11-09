044707338f45c4b3c97c6b15d1013e63d399ab72
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-11-09 22:10:22 +0000

    refactor code

--- script.js
+++ script.js
@@ -7,21 +7,19 @@ const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
-// Show Loading
-function loading() {
+function showLoadingSpinner() {
     loader.hidden = false;
     quoteContainer.hidden = true;
 }
 
-// Hide Loading
-function complete() {
+function removeLoadingSpinner() {
     quoteContainer.hidden = false;
     loader.hidden = true;
 }
 
 // Show New Quote
 function newQuote() {
-    loading();
+    showLoadingSpinner();
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
@@ -36,14 +34,12 @@ function newQuote() {
     }
 
     quoteText.textContent = quote.text;
-
-    // Hide Loader
-    complete();
+    removeLoadingSpinner();
 };
 
 // Get Quotes From API
 async function getQuotes() {
-    loading();
+    showLoadingSpinner();
     const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
     
     try {
